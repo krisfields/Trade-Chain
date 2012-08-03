df173bbb552af65f1ccb214cc273cf79226a87c4
Kris Fields <kris@avalaunch.com>
2012-08-03 13:58:44 -0700

    rails_apps_composer: initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+TradeChain::Application.config.secret_token = '87845e6ccab59dcdf0a9079edc1a199db59db0a18065ad61a9d8d318dd24237c74f75cf00eae6c2b685aaebcfce56c0c8016f9e96ab82bf0a87aaf3e112896d4'
